bc7b2a9d79afe87621e071cc76d3354735b7caae
Daniil Miskevich <daniilmiskevich@gmail.com>
2023-06-06 18:15:26 +0300

    Moved animation durations into the `MyTheme`.

--- lib/core/presentation/my_theme.dart
+++ lib/core/presentation/my_theme.dart
@@ -1,12 +1,23 @@
 import "package:flutter/material.dart";
 
 abstract class MyTheme {
-  static const double smallSpaceSize = 10;
-  static const double bigSpaceSize = 20;
+  // sizes
+
+  static const double smallSpaceSize = 10, bigSpaceSize = 20;
 
   static const double pageMaxWidth = 720;
 
+  // paddings
+
   static const EdgeInsets cardPadding = EdgeInsets.all(24);
+
+  // animations
+
+  static const subpageTransitionDuration = Duration(milliseconds: 300);
+  static const subpageTransitionInCurve = Curves.easeOutCubic;
+  static const subpageTransitionOutCurve = Curves.easeInExpo;
+
+  // theme data
 
   static final theme = ThemeData(
     useMaterial3: true,
